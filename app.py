d872876fa16419dceae319c200647682cd00f1ee
Chawala Banda <chawala.banda@keio.jp>
2026-01-14 21:23:31 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Neurodiversity App", page_icon="🌿", layout="wide")
+
+st.title("Neurodiversity Communication Toolkit")
+st.write("Deployed successfully ✅")
+st.info("Next: we’ll add Framework, Toolkit, Feedback Tool, and About pages.")
