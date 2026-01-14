3af57a53971ecfaaf014644890f8cdeb691a762e
Chawala Banda <chawala.banda@keio.jp>
2026-01-14 22:03:54 +0900

    Update and rename 2_Prototype_Toolkit.py to 2_Phrases_and_Scripts.py

---
+++ pages/2_Phrases_and_Scripts.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.title("Phrases & Scripts")
+st.write("Phrases & Scripts content goes here.")
--- pages/2_Prototype_Toolkit.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-st.title("Prototype Toolkit")
-st.write("Toolkit content goes here.")
