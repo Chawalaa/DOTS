a8b7a65c3184af4ae5ee3709ce9652650a782149
Chawala Banda <chawala.banda@keio.jp>
2026-01-18 00:34:41 +0900

    Update 4_Guides.py

--- pages/4_Guides.py
+++ pages/4_Guides.py
@@ -2,7 +2,6 @@ import streamlit as st
 from components.ui import apply_brand_styles, set_sidebar_branding, language_toggle, get_lang, page_header
 
 st.set_page_config(page_title="Guides", layout="wide")
-language_toggle(sidebar=True)
 
 from pathlib import Path
 
